24ab4b13ae58f93ebbe00c63823f24ce0e57a34a
Steph <stepsen.dev@gmail.com>
2021-01-08 18:03:16 +0000

    using jwt webtoken

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "devadmin",
+    "password": "udasen89luna",
+    "database": "udagramsenonerdev",
+    "host": "udagramsenonerdev.ci3l8yebrxjn.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-senoner-dev",
+    "jwt-secret": "helloworld"
   },
   "prod": {
     "username": "",
