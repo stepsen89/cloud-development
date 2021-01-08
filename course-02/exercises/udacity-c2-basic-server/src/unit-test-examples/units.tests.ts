d54de8106f34504046bf5e814837627793e6fd41
Steph <stepsen.dev@gmail.com>
2021-01-08 17:39:37 +0000

    example unit tests for c2 basic server

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -6,12 +6,12 @@ import 'mocha';
 describe('add function', () => {
 
   it('should add two and two', () => {
-    const result = add(2,2);
+    const result = add(2, 2);
     expect(result).to.equal(4);
   });
 
   it('should add -2 and two', () => {
-    const result = add(-2,2);
+    const result = add(-2, 2);
     expect(result).to.equal(0);
   });
 
@@ -20,21 +20,33 @@ describe('add function', () => {
 describe('divide', () => {
 
   it('should divide 6 by 3', () => {
-    const result = divide(6,3);
+    const result = divide(6, 3);
     expect(result).to.equal(2);
   });
 
   it('should divide 5 and 2', () => {
-    const result = divide(5,2);
+    const result = divide(5, 2);
     expect(result).to.equal(2.5);
   });
 
   it('should throw an error if div by zero', () => {
-    expect(()=>{ divide(5,0) }).to.throw('div by 0')
+    expect(() => { divide(5, 0) }).to.throw('div by 0')
   });
 
 });
 
+describe('concat', () => {
+
+  it('should concat test and string', () => {
+    const result = concat('test', 'string');
+    expect(result).to.equal('teststring');
+  });
+
+  it('should throw an error if either string is empty', () => {
+    expect(() => { concat('', '') }).to.throw('Empty string')
+  });
+
+});
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -2,16 +2,18 @@
 
 export const add = (a: number, b: number) => {
     return a + b;
-    }
+}
 
 export const divide = (a: number, b: number) => {
-    if(b === 0) { throw new Error('div by 0') }
+    if (b === 0) { throw new Error('div by 0') }
 
     return a / b;
+}
+
+export const concat = (pre: string, post: string) => {
+    if (!pre || !post || pre.length === 0 || post.length === 0) {
+        throw new Error('Empty string')
     }
 
-// @TODO try creating a method "concat" to concatenate two strings
-// it should take two string paramaters.
-// it should return one string combining the two strings.
-// it should throw an error if either of the strings are empty.
-// ensure your function is exported.
+    return pre + post;
+}
